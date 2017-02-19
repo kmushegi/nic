6731e177e1e859236c285f7bec8b8f4fa9113f97
Marcus <mefchristiansen@gmail.com>
2017-02-18 22:33:43 -0500

    GA working for ts with 1p and uc

--- p1/EvolAlg.java
+++ p1/EvolAlg.java
@@ -17,10 +17,19 @@ class Child {
 	public static ArrayList<Integer> child1;
 	public static ArrayList<Integer> child2;
 
-	// public static void Child() {
-	// 	child1 = new ArrayList<>();
-	// 	child2 = new ArrayList<>();
-	// }
+	Child() {
+		child1 = new ArrayList<>();
+		child2 = new ArrayList<>();
+	}
+
+	void addToChild1(int newInt) {
+		child1.add(newInt);
+	}
+
+	void addToChild2(int newInt) {
+		child2.add(newInt);
+	}
+
 }
 
 class rankedIndividual {
@@ -100,7 +109,6 @@ public class EvolAlg {
 		//Fitness = number of clauses met
 		//Space = or
 		//negative = not
-
 		int fitness = 0;
 
 		for (int i = 0; i < formula.size(); i++) { //For each clause
@@ -126,7 +134,23 @@ public class EvolAlg {
 			}
 			pp.add(temp);
 		}
+
 		return pp;
+	}
+
+	public static int bestSolutionIndex(ArrayList<Integer> fitnessEvaluations) {
+
+		int bestSolutionIndex = 0;
+		int currentMax = fitnessEvaluations.get(0);
+		for(int i = 1; i < fitnessEvaluations.size(); i++) {
+			if(fitnessEvaluations.get(i) > currentMax) {
+				bestSolutionIndex = i;
+				currentMax = fitnessEvaluations.get(i);
+			}
+		}
+
+		return bestSolutionIndex;
+
 	}
 
 	public static ArrayList<Integer> ga(int indInPopulation, String selection,
@@ -140,39 +164,41 @@ public class EvolAlg {
 			fitnessEvaluations.add(evaluateFitness(population.get(i)));
 		}
 
-		int bestSolutionIndex = 0;
-		int currentMax = fitnessEvaluations.get(0);
-		for(int i = 1; i < fitnessEvaluations.size(); i++) {
-			if(fitnessEvaluations.get(i) > currentMax) {
-				bestSolutionIndex = i;
-				currentMax = fitnessEvaluations.get(i);
-			}
-		}
-
-		ArrayList<Integer> best = population.get(bestSolutionIndex);
+		ArrayList<Integer> best = population.get(bestSolutionIndex(fitnessEvaluations));
 
 		ArrayList<ArrayList<Integer>> parents = new ArrayList<>();
-		ArrayList<ArrayList<Integer>> children = new ArrayList<>();
 
 		while(numGenerations > 0) {
 
+			// System.out.println("Parents Size: " + parents.size());
+
 			//Select parents
-			if(selection.equals("rs")) {
-				//rank selection
+			if(selection.equals("rs")) { //rank selection
 				parents = rs(population);
-			} else if(selection.equals("ts")) {
-				//tournament selection
+			} 
+			else if(selection.equals("ts")) { //tournament selection
 				parents = ts(population);
-			} else if(selection.equals("bs")) {
-				//boltzmann selection
+			}
+			else if(selection.equals("bs")) { //boltzmann selection
 				parents = bs(population);
-			} else {
+			}
+			else {
 				System.exit(1);
 			}
 
-			for (int i = 0; i < parents.size(); i+=2) { //Hope that array is even!
+			// System.out.println("Parents Size: " + parents.size());
 
-				Child newChildren = new Child();
+			// for (int i = 0; i < parents.size(); i++) {
+			// 	System.out.println("Parent " + i + " Size: " + parents.get(i).size());
+			// }
+			// System.out.println();
+
+			// System.out.println("Parents Size: " + parents.size());
+
+			ArrayList<ArrayList<Integer>> children = new ArrayList<>();
+			Child newChildren = new Child();
+
+			for (int i = 0; i < parents.size(); i+=2) {
 				if (crossover.equals("1c")) {
 					newChildren = onepoint(parents.get(i), parents.get(i+1), crossoverProbability);
 				} else if (crossover.equals("uc")) {
@@ -186,25 +212,35 @@ public class EvolAlg {
 
 				c1 = mutateChild(c1);
 				c2 = mutateChild(c2);
+
+				children.add(c1); //Add children to children array list
+				children.add(c2);
+
+				// System.out.println(c1);
+				// System.out.println(c2);
+
+				// System.out.println("Children Size: " + children.size());
+
 			}
 
-			ArrayList<Integer> childrenFitnessEvaluations = new ArrayList<>(children.size());
+			// System.out.println("Children Size: " + children.size());
+
+			// for (int i = 0; i < children.size(); i++) {
+			// 	System.out.println(children.get(i));
+			// }
+
+
+			ArrayList<Integer> childrenFitnessEvaluations = new ArrayList<>();
 			for(int i = 0; i < children.size(); i++) {
-				fitnessEvaluations.set(i,evaluateFitness(children.get(i)));
+				childrenFitnessEvaluations.add(evaluateFitness(children.get(i)));
+				// System.out.println()
 			}
 
-			bestSolutionIndex = 0;
-			currentMax = childrenFitnessEvaluations.get(0);
-			for(int i = 1; i < childrenFitnessEvaluations.size(); i++) {
-				if(childrenFitnessEvaluations.get(i) > currentMax) {
-					bestSolutionIndex = i;
-					currentMax = childrenFitnessEvaluations.get(i);
-				}
-			}
-			
-			best = children.get(bestSolutionIndex);
+			best = population.get(bestSolutionIndex(childrenFitnessEvaluations));
 
 			population = children;
+
+			// System.out.println("Population Size: " + population.size());
 
 			numGenerations--;
 		}
@@ -223,6 +259,8 @@ public class EvolAlg {
 			}
 		}
 
+		// System.out.println("Child Size: " + child.size());
+
 		return child;
 	}
 
@@ -230,40 +268,60 @@ public class EvolAlg {
 												double crossoverProbability) {
 		Child c = new Child();
 
-		if (generator.nextDouble() > crossoverProbability){
+		if (generator.nextDouble() > crossoverProbability) {
 			c.child1 = parent1;
 			c.child2 = parent2;
+			return c;
 		}
-		else {
-			int randIndex = generator.nextInt(parent1.size());
 
-			for(int i = 0; i < parent1.size(); i++){
-				c.child1.set(i , parent1.get(i));
-				c.child2.set(i , parent2.get(i));
-				if(i >= randIndex){
-					int cross1 = parent1.get(i);
-					int cross2 = parent2.get(i);
-					c.child1.set(i , cross2);
-					c.child2.set(i , cross1);
-				}
+		int randIndex = generator.nextInt(parent1.size());
+		for(int i = 0; i < parent1.size(); i++){
+			if(i >= randIndex){
+				c.addToChild1(parent2.get(i));
+				c.addToChild2(parent1.get(i));
+			}
+			else {
+				c.addToChild1(parent1.get(i));
+				c.addToChild2(parent2.get(i));
 			}
 		}
+
+		// System.out.println("Child 1 Size: " + c.child1.size());
+		// System.out.println("Child 2 Size: " + c.child2.size());
+
 		return c;
 	}
 
+	//Check is cross over.
+	//Equal prob pick from parent.
 	public static Child uniform(ArrayList<Integer> parent1, ArrayList<Integer> parent2, 
 												double crossoverProbability) {
 		Child c = new Child();
 
-		for(int i = 0; i < parent1.size(); i++){
+		if (generator.nextDouble() > crossoverProbability) {
+			c.child1 = parent1;
+			c.child2 = parent2;
+			return c;
+		}
 
-			if(generator.nextDouble() < crossoverProbability){
-					int cross1 = parent1.get(i);
-					int cross2 = parent2.get(i);
-					c.child1.set(i , cross2);
-					c.child2.set(i , cross1);
+		for(int i = 0; i < parent1.size(); i++){
+			if(generator.nextDouble() > 0.5){
+				c.addToChild1(parent1.get(i));
+			}
+			else {
+				c.addToChild1(parent2.get(i));
 			}
 		}
+
+		for(int i = 0; i < parent1.size(); i++){
+			if(generator.nextDouble() > 0.5){
+				c.addToChild2(parent1.get(i));
+			}
+			else {
+				c.addToChild2(parent2.get(i));
+			}
+		}
+
 		return c;
 	}
 
@@ -313,7 +371,6 @@ public class EvolAlg {
 
 	//M = 2, K = 1
 	public static ArrayList<ArrayList<Integer>> ts(ArrayList<ArrayList<Integer>> population) {
-		
 		ArrayList<ArrayList<Integer>> selected = new ArrayList<>();
 
 		while(selected.size() < population.size()) {
@@ -364,15 +421,23 @@ public class EvolAlg {
 		ArrayList<ArrayList<Integer>> samples = new ArrayList<>(indPerIteration);
 		ArrayList<Integer> fitnessEvaluations = new ArrayList<>(); //The higher the value, the better the fitness.
 
+		int mostFit = 0;
+
 		while(numIterations > 0) {
 			//generate individuals
 			for(int i = 0; i < indPerIteration; i++) { //for each individual
 				ArrayList<Integer> sample = new ArrayList<>();
 				for(int j = 0; j < numberOfVariables; j++) { //for each variable
+
+					// double random = generator.nextDouble();
+					// System.out.println("Random: " + random);
+
 					sample.add(((generator.nextDouble() > probVector.get(j)) ? 0 : 1));
 				}
+
 				samples.add(sample);
 				fitnessEvaluations.add(evaluateFitness(samples.get(i)));
+				// System.out.println("Fitness: " + fitnessEvaluations.get(i));
 			}
 
 			int bestVectorIndex, worstVectorIndex;
@@ -380,17 +445,37 @@ public class EvolAlg {
 
 			for (int i = 1; i < fitnessEvaluations.size(); i++) {
 				if (fitnessEvaluations.get(i) < fitnessEvaluations.get(worstVectorIndex)) {
+					// System.out.println("Worse fitness: " + fitnessEvaluations.get(worstVectorIndex) + ">" + fitnessEvaluations.get(i));
 					worstVectorIndex = i;
 				}
 				if (fitnessEvaluations.get(i) > fitnessEvaluations.get(bestVectorIndex)) {
+					// System.out.println("Better fitness: " + fitnessEvaluations.get(bestVectorIndex) + "<" + fitnessEvaluations.get(i));
 					bestVectorIndex = i;
 				}
 			}
+
+			if (mostFit != fitnessEvaluations.get(bestVectorIndex)) {
+				mostFit = fitnessEvaluations.get(bestVectorIndex);
+				// System.out.println("FITNESS CHANGE");
+			}
+
+			// System.out.println("Most fit: " + fitnessEvaluations.get(bestVectorIndex));
+			// System.out.println("Least fit: " + fitnessEvaluations.get(worstVectorIndex));
+
+			// System.out.println("Prob Vector: ");
+			// for(int i = 0; i < numberOfVariables; i++) {
+			// 	System.out.println(probVector.get(i));
+			// }
 
 			for (int i = 0; i < numberOfVariables; i++) {
 				probVector.set(i,(probVector.get(i) * (1.0 - posLearningRate) + 
 								(samples.get(bestVectorIndex).get(i) * posLearningRate)));
 			}
+
+			// System.out.println("Prob Vector: ");
+			// for(int i = 0; i < numberOfVariables; i++) {
+			// 	System.out.println(probVector.get(i));
+			// }
 
 			for (int i = 0; i < numberOfVariables; i++) {
 				if (samples.get(bestVectorIndex).get(i) != samples.get(worstVectorIndex).get(i)) {
@@ -408,6 +493,27 @@ public class EvolAlg {
 			}
 
 			numIterations--;
+
+			// ArrayList<Integer> processed = new ArrayList<>();
+
+			// // System.out.println("Prob vector");
+
+			// for(int i = 0; i < probVector.size(); i++) {
+
+			// 	// System.out.println(probVector.get(i));
+
+			// 	if(probVector.get(i) > 0.5) {
+			// 		processed.add(1);
+			// 	} else {
+			// 		processed.add(0);
+			// 	}
+			// }
+
+			// System.out.println();	
+			// System.out.println();		
+
+			// int satisfied = evaluateFitness(processed);
+			// System.out.println("# of Satisfied Clauses: " + satisfied);
 
 		}
 		
