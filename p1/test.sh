e366e7ca6582fb05935fe85ea1151b8d73de3c8f
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-02-20 18:53:19 -0500

    updated script to run programs on all files in a given folder

--- p1/test.sh
+++ p1/test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-make p > out.txt #p or g to be supplied as a parameter
+#parameters to script pbil or ga
+
+FILES="maxsat-problems/maxsat-random/highgirth/5SAT/*.cnf"
+
+for f in $FILES
+do
+	if [ "$1" == "pbil" ]; then
+		java EvolAlg $f 100 0.1 0.075 0.02 0.05 1000 p > out.txt
+	elif [ "$1" == "ga" ]; then
+		java EvolAlg $f 100 bs uc 0.7 0.01 1000 g > out.txt
+	else
+		echo "Algorithm specified incorrectly. Choose pbil or ga"
+		exit 1
+	fi
+done
 
 if [ ! -f out.txt ]; then
 	echo "Output File Not Found!"
