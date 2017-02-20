deb81abdf83b9e9e85d526ddd95ea735b768992a
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-02-20 12:28:04 -0500

    add dummy out & sol files, add shell script to write output to file and compare two files (works for text & binary). if we notice that the comparison fails we could use diff commant which is specifically made for text files

--- p1/test.sh
+++ p1/test.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-make p > output.txt
+make p > out.txt
+
+if [ ! -f out.txt ]; then
+	echo "Output File Not Found!"
+	exit 1
+fi
+
+if [ ! -f sol.txt ]; then
+	echo "Solution File Not Found!"
+	exit 1
+fi
+
+if cmp -s out.txt sol.txt
+then
+	echo "The files match"
+else
+	echo "The files do not match"
+fi
