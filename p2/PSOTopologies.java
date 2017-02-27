d42ffe639a69ffbcecfcf837ed07c0df9923f66b
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-02-27 09:31:33 -0500

    add basic io

---
+++ p2/PSOTopologies.java
@@ -0,0 +1,49 @@
+/*
+Comparison of PSO Topologies - Project 2
+Nature Inspired Computation
+Spring 2017
+Stephen Majercik
+
+Ernesto Garcia, Marcus Christiansen, Konstantine Mushegian
+
+The code in this file contains the implementation of A Comparison of 
+Neighborhood Topologies in Particle Swarm Optimization, as part of Project 2.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class PSOTopologies {
+
+	//Parameters in order of acceptance from CL
+	private static String whichTopology;
+	private static int swarmSize;
+	private static int numberOfIterations = 10000; //fixed but can be specifed on CL
+	private static String whichFunction;
+	private static int functionDimensionality = 30; //fixed but can be specifed on CL
+
+	public static void main(String[] args) {
+		if(args.length == 3) {
+			whichTopology = args[0];
+			swarmSize = Integer.parseInt(args[1]);
+			whichFunction = args[2];
+		} else if(args.length == 5) {
+			whichTopology = args[0];
+			swarmSize = Integer.parseInt(args[1]);
+			numberOfIterations = Integer.parseInt(args[2]);
+			whichFunction = args[3];
+			functionDimensionality = Integer.parseInt(args[4]);
+		} else {
+			System.exit(1); //exit with error
+		}
+		printParams();
+	}
+
+	public static void printParams() {
+		System.out.println("Topology: " + whichTopology
+			+ "\nSwarm Size: " + swarmSize
+			+ "\n# of Iterations: " + numberOfIterations
+			+ "\nFunction: " + whichFunction
+			+ "\nFunction Dimensionality: " + functionDimensionality);
+	}
+}
