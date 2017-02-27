870c8fa7bbf32c2e67fc760102b90431964a79d0
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-02-27 09:56:42 -0500

    fix the error typo again. ugh. reformat param reading in PSO

--- p2/PSOTopologies.java
+++ p2/PSOTopologies.java
@@ -22,8 +22,21 @@ public class PSOTopologies {
 	private static String whichFunction;
 	private static int functionDimensionality = 30; //fixed but can be specifed on CL
 
+	//Messages
+	private static String incorrectParams = "Parameters were not supplied correctly. Refer to man below:";
+	private static String paramsOrder = "Param Order: gl/ri/vn/ra swarmSize numIterations rok/ack/ras funcDimensionality";
+
 	public static void main(String[] args) {
-		if(args.length == 3) {
+		readParams(args);
+		printParams();
+	}
+
+	public static void readParams(String[] args) {
+		if(args.length != 3 && args.length != 5) {
+			System.out.println(incorrectParams);
+			System.out.println(paramsOrder);
+			System.exit(1); //exit with error
+		} else if(args.length == 3) {
 			whichTopology = args[0];
 			swarmSize = Integer.parseInt(args[1]);
 			whichFunction = args[2];
@@ -33,11 +46,7 @@ public class PSOTopologies {
 			numberOfIterations = Integer.parseInt(args[2]);
 			whichFunction = args[3];
 			functionDimensionality = Integer.parseInt(args[4]);
-		} else {
-			System.out.println("Parameters were not supplied incorrectly");
-			System.exit(1); //exit with error
 		}
-		printParams();
 	}
 
 	public static void printParams() {
