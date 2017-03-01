5f64aadcefba0b03d8d1bdc2ae6fb36bae112082
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-03-01 12:25:41 -0500

    add bunch of initialization framework

--- p2/PSOTopologies.java
+++ p2/PSOTopologies.java
@@ -14,48 +14,60 @@ import java.io.*;
 import java.util.*;
 
 class Particle {
-	public int x;
-	public int y;
-	public int v;
+	public ArrayList<Double> location;
+	public ArrayList<Double> velocity;
+	public ArrayList<Integer> neighbors; //stored by particle number, i?
 
-	Particle() {
-		x = 0;
-		y = 0;
-		v = 0;
+	public ArrayList<Double> personalBestLocation;
+	public double personalBestValue;
+
+	public ArrayList<Double> neighborhoodBestLocation;
+	public double neighborhoodBestValue;
+
+	public int dim;
+
+	private static Random generator = new Random();
+
+	Particle(int d) {
+		this.location = new ArrayList<>(d);
+		this.velocity = new ArrayList<>(d);
+		this.personalBestLocation = new ArrayList<>(d);
+		this.neighborhoodBestLocation = new ArrayList<>(d);
+		this.dim = d;
 	}
 
-	Particle(int nx, int ny, int nv) {
-		x = nx;
-		y = ny;
-		v = nv;
+	public Particle initializeParticle(double minSpeed, double maxSpeed,
+									double minLocation, double maxLocation,
+									double bound) {
+		for(int i = 0; i < this.dim; i++) {
+			double x_i = bound * generator.nextDouble();
+
+			while(x_i < minLocation || x_i > maxLocation) {
+				x_i = bound * generator.nextDouble();
+			}
+
+			this.location.add(x_i);
+			//this.velocity = ; //initialize velocity
+			this.personalBestValue = -Double.MAX_VALUE;
+			this.neighborhoodBestValue = -Double.MAX_VALUE;
+		}
+		personalBestLocation = location;
+		return this;
 	}
 
-	void setX(int nx) {
-		x = nx;
+	public void setNeighborhood(ArrayList<Integer> n) {
+		this.neighbors = n;
 	}
 
-	void setY(int ny) {
-		y = ny;
-	}
-
-	void setV(int nv) {
-		v = nv;
-	}
-
-	int getX() {
-		return x;
-	}
-
-	int getY() {
-		return y;
-	}
-
-	int getV() {
-		return v;
+	public int getDimension() {
+		return dim;
 	}
 }
 
 public class PSOTopologies {
+
+	private static final double CONSTRICTION_FACTOR = 0.7298;
+	private static final double BOUND = 500; //Generalized Schwefel 2.6
 
 	//Parameters in order of acceptance from CL
 	//some parameters are fixed byt can be specified on CL
@@ -64,6 +76,15 @@ public class PSOTopologies {
 	private static int numberOfIterations = 10000;
 	private static String whichFunction;
 	private static int functionDimensionality = 30;
+
+	//Params
+	private static double minVelocity;
+	private static double maxVelocity;
+	private static double minLocation;
+	private static double maxLocation;
+
+	//Containers
+	private static ArrayList<Particle> particles;
 
 	//Messages
 	private static String incorrectParams = "Parameters were not supplied " 
@@ -75,13 +96,58 @@ public class PSOTopologies {
 	public static void main(String[] args) {
 		readParams(args);
 		printParams();
+		initializeBounds(whichFunction);
+		initializeParticles();
+		initializeTopology(whichTopology);
+	}
+
+	public static void initializeTopology(String topology) {
+		if(topology.equals("gl")) {
+			//init global topology, these should be separate functions
+		} else if(topology.equals("ri")) {
+			//init ring topology
+		} else if(topology.equals("vn")) {
+			//init von neumann topology
+		} else if(topology.equals("ra")) {
+			//init random topology
+		} else {
+			printErrorAndExit();
+		}
+	}
+
+	public static void initializeParticles() {
+		for(int i = 0; i < swarmSize; i++) {
+			Particle t = new Particle(functionDimensionality);
+			t.initializeParticle(minVelocity, maxVelocity, 
+								minLocation, maxLocation, BOUND);
+			particles.add(t);
+		}
+	}
+
+	public static void initializeBounds(String function) {
+		if(function.equals("rok")) {
+			minLocation = 15.0;
+			maxLocation = 30.0;
+			minVelocity = -2.0;
+			maxVelocity = 2.0;
+		} else if(function.equals("ack")) {
+			minLocation = 16.0;
+			maxLocation = 32.0;
+			minVelocity = -2.0;
+			maxVelocity = 4.0;
+		} else if(function.equals("ras")) {
+			minLocation = 2.56;
+			maxLocation = 5.12;
+			minVelocity = -2.0;
+			maxVelocity = 4.0;
+		} else {
+			printErrorAndExit();
+		}
 	}
 
 	public static void readParams(String[] args) {
 		if(args.length != 3 && args.length != 5) {
-			System.out.println(incorrectParams);
-			System.out.println(paramsOrder);
-			System.exit(1); //exit with error
+			printErrorAndExit();
 		} else if(args.length == 3) {
 			whichTopology = args[0];
 			swarmSize = Integer.parseInt(args[1]);
@@ -93,6 +159,12 @@ public class PSOTopologies {
 			whichFunction = args[3];
 			functionDimensionality = Integer.parseInt(args[4]);
 		}
+	}
+
+	public static void printErrorAndExit() {
+		System.out.println(incorrectParams);
+		System.out.println(paramsOrder);
+		System.exit(1); //exit with error
 	}
 
 	public static void printParams() {
