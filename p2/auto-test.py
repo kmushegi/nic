8d0512f5acc33da588946bb9703ea20600888bbd
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-03-08 15:43:56 -0500

    add test script, add stats folder w dummy .txt file inside

---
+++ p2/auto-test.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+def compile():
+	cmd = "javac -Werror -Xlint:all PSOTopologies.java Particle.java"
+	os.system(cmd)
+
+def runPSO(whichTopology, swarmSize, iterations, whichFunction, dimensions):
+	cmd = "java PSOTopologies " + whichTopology + " " + str(swarmSize) + " " + \
+			str(iterations) + " " + whichFunction + " " + str(dimensions) + \
+			" >> stats/pso-" + whichTopology + "-" + str(swarmSize) + \
+			"-" + str(iterations) + "-" + whichFunction + "-" + str(dimensions) + ".txt"
+	os.system(cmd)
+
+
+compile()
+
+swarm_size = [16,30,49]
+topology = ["gl","ri","vn","ra"]
+functions = ["rok","ack","ras"]
+
+dimensions = 30
+iterations = 10000
+runs = 20
+
+for s in swarm_size:
+	for t in topology:
+		for f in functions:
+			runPSO(t,s,iterations,f,dimensions)
