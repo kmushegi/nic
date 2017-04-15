ef3eb6e9f81c46eeb29daf1adb514dd1a7a55593
Marcus <mefchristiansen@gmail.com>
2017-04-14 22:56:41 -0400

    final

--- p3/auto-test.py
+++ p3/auto-test.py
@@ -49,11 +49,12 @@ if parallel:
 	Parallel(n_jobs=nj)(delayed(run)(whichAlgorithm=whichAlgorithm,numAnts=ni,numIts=ii,alpha=ai,beta=bi,rho=ri,pfp=fpi,stopCond=0,secAllowed=10,errAllowed=0.1,eps=ei,qZero=qi) for ni in numAnts for ii in numIts for ai in alphas for bi in betas for ri in rhos for ei in epsilons for qi in qZeroes for fpi in problems)
 	print "Done"
 else:
-	for n in numAnts:
-		for f in os.listdir(problemdir):
-			if f.endswith(".tsp"):
-				fp = "../" + problemdir + "/" + f
-				run(whichAlgorithm,n,200,1,4,0.1,fp,0,10,0.1,0.1,0.9)
+	for i in range(0,3):
+		for n in numAnts:
+			for f in os.listdir(problemdir):
+				if f.endswith(".tsp"):
+					fp = "../" + problemdir + "/" + f
+					run(whichAlgorithm,n,200,1,4,0.1,fp,0,10,0.1,0.1,0.9)
 									
 
 
