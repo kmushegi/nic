0503641e3c981cae14e4e2110b5b057d2130d4ee
Marcus <mefchristiansen@gmail.com>
2017-04-14 23:06:26 -0400

    final 3

--- p3/auto-test.py
+++ p3/auto-test.py
@@ -39,22 +39,22 @@ clear_stats_folder()
 numAnts = [30]
 
 whichAlgorithm = sys.argv[1]
-parallel = int(sys.argv[2])
+# parallel = int(sys.argv[2])
 
 problems = os.listdir(problemdir)
 
-if parallel:
-	nj = multiprocessing.cpu_count()
-	print "Spawning " + str(nj) + " jobs"
-	Parallel(n_jobs=nj)(delayed(run)(whichAlgorithm=whichAlgorithm,numAnts=ni,numIts=ii,alpha=ai,beta=bi,rho=ri,pfp=fpi,stopCond=0,secAllowed=10,errAllowed=0.1,eps=ei,qZero=qi) for ni in numAnts for ii in numIts for ai in alphas for bi in betas for ri in rhos for ei in epsilons for qi in qZeroes for fpi in problems)
-	print "Done"
-else:
-	for i in range(0,3):
-		for n in numAnts:
-			for f in os.listdir(problemdir):
-				if f.endswith(".tsp"):
-					fp = "../" + problemdir + "/" + f
-					run(whichAlgorithm,n,200,1,4,0.1,fp,0,10,0.1,0.1,0.9)
+# if parallel:
+# 	nj = multiprocessing.cpu_count()
+# 	print "Spawning " + str(nj) + " jobs"
+# 	Parallel(n_jobs=nj)(delayed(run)(whichAlgorithm=whichAlgorithm,numAnts=ni,numIts=ii,alpha=ai,beta=bi,rho=ri,pfp=fpi,stopCond=0,secAllowed=10,errAllowed=0.1,eps=ei,qZero=qi) for ni in numAnts for ii in numIts for ai in alphas for bi in betas for ri in rhos for ei in epsilons for qi in qZeroes for fpi in problems)
+# 	print "Done"
+# else:
+for i in range(0,3):
+	for n in numAnts:
+		for f in os.listdir(problemdir):
+			if f.endswith(".tsp"):
+				fp = "../" + problemdir + "/" + f
+				run(whichAlgorithm,n,200,1,4,0.1,fp,0,10,0.1,0.1,0.9)
 									
 
 
