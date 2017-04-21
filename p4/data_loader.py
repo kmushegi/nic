93e28df31b00e5ba1f06a282662306a5472968c5
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-04-21 16:59:59 -0400

    read_data takes a parameter, read both test and training files, read_data returns a tuple

--- p4/data_loader.py
+++ p4/data_loader.py
@@ -2,9 +2,10 @@ import numpy as np
 import sys
 
 training_data_file = "digit-recognition-data/32x32-bitmaps/optdigits-32x32.tra"
+testing_data_file = "digit-recognition-data/32x32-bitmaps/optdigits-32x32.tes"
 
-def read_data():
-	f = open(training_data_file)
+def read_data(data_file):
+	f = open(data_file)
 
 	for i in xrange(3): #skip first 3 lines
 		next(f)
@@ -25,4 +26,7 @@ def read_data():
 			current_input = ""
 			counter = 0
 
-read_data()
+	return (inputs, desired_outputs)
+
+(tra_inputs,tra_desired_outputs) = read_data(training_data_file)
+(tes_inputs,tes_desired_outputs) = read_data(testing_data_file)
