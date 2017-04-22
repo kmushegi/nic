c324181feaab3a4a9eb43bdd71cc45f62ff5c63d
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-04-22 12:55:27 -0400

    some more progress with data loading

--- p4/data_loader.py
+++ p4/data_loader.py
@@ -17,7 +17,7 @@ def read_data(data_file):
 	current_input = ""
 	for line in f:
 		if(counter < 32):
-			current_input += line
+			current_input += line.strip()
 			counter += 1
 		else:
 			inputs.append(current_input)
@@ -28,5 +28,24 @@ def read_data(data_file):
 
 	return (inputs, desired_outputs)
 
-(tra_inputs,tra_desired_outputs) = read_data(training_data_file)
-(tes_inputs,tes_desired_outputs) = read_data(testing_data_file)
+def format_data_10_output_nodes():
+
+	(tr_i, tr_o) = read_data(training_data_file)
+	training_inputs = [np.reshape(list(x), (1024, 1)) for x in tr_i]
+	training_outputs = [vectorize_output(x) for x in tr_o]
+	training_data = zip(training_inputs,training_outputs)
+
+	(te_i,te_o) = read_data(testing_data_file)
+	testing_inputs = [np.reshape(list(x),(1024,1)) for x in te_i]
+	testing_outputs = [vectorize_output(x) for x in te_o]
+	testing_data = zip(testing_inputs,testing_outputs)
+
+	return (training_data, testing_data)
+
+def vectorize_output(j):
+	e = np.zeros((10,1))
+	e[j] = 1.0
+	return e
+
+(tr_data, te_data) = format_data_10_output_nodes()
+print(len(tr_data))
