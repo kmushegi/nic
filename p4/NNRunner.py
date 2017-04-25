c81ca741c479e588f0c610e951c16a46e4c2f8f3
Marcus <mefchristiansen@gmail.com>
2017-04-25 18:39:23 -0400

    small additions

--- p4/NNRunner.py
+++ p4/NNRunner.py
@@ -4,6 +4,6 @@ import numpy
 
 (training_data, test_data) = dl.format_data_10_output_neurons()
 
-net = Network.Network(1024,10,1,1)
+net = Network.Network(1024,10,1,0.01)
 net.train(training_data)
 
