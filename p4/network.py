256be7e7ffff150459f08970608a6cb4c796b783
Marcus <mefchristiansen@gmail.com>
2017-04-26 19:12:08 -0400

    may be working now

--- p4/network.py
+++ p4/network.py
@@ -12,18 +12,21 @@ class Network(object):
 		self.num_outputs = num_outputs
 		self.num_epochs = num_epochs
 		self.learning_rate = learning_rate
-
 		self.in_activations = [1.0] * self.num_inputs
 		self.out_activations = [1.0] * self.num_outputs
-
-		self.weights = np.zeros((self.num_inputs, self.num_outputs)) #np.random.rand(self.num_inputs, self.num_outputs) * 0.15
+		#Negative weights between -0.15 and 0.15
+		self.weights = 0.3 * np.random.rand(self.num_inputs, self.num_outputs) - 0.15
+		self.inputSums = [1.0] * self.num_outputs
 
 	def train(self, training_data, test_data=None):
 		for i in xrange(self.num_epochs):
+			j = 1
 			for trainingSet in training_data:
-				self.initializein_activations(trainingSet[0])
+				print("New Training " + str(j))
+				self.initialize_in_activations(trainingSet[0])
 				self.calculateOutputNodeValues()
 				self.updateWeights(trainingSet[1])
+				j+=1
 			print "Epoch: {0} done.".format(i)
 		if test_data:
 			self.evaluate(test_data)
@@ -36,14 +39,15 @@ class Network(object):
 		output = self.sigmoid(self.sumInputs(i))
 		return output
 
-	def initializein_activations(self, trainingInput):
+	def initialize_in_activations(self, trainingInput):
 		for i in range(trainingInput.size):
 			self.in_activations[i] = 1.0 * int(trainingInput.item(i))
 		self.in_activations[-1] = 1.0 #Bias node
 
 	def calculateOutputNodeValues(self):
 		for i in range(len(self.out_activations)):
-			self.out_activations[i] = self.sigmoid(self.sumInputs(i))
+			self.inputSums[i] = self.sumInputs(i)
+			self.out_activations[i] = self.sigmoid(self.inputSums[i])
 			print(self.out_activations[i])
 
 	def sumInputs(self, index):
@@ -53,7 +57,7 @@ class Network(object):
 		return inputSum
 
 	def sigmoid(self, x):
-		return 1.0 / (1.0 + np.exp(-x + 0.5))
+		return 1.0 / (1.0 + np.exp(-x))
 
 	def sigmoidPrime(self, x):
 		return self.sigmoid(x) * (1.0 - self.sigmoid(x))
@@ -61,14 +65,11 @@ class Network(object):
 	def updateWeights(self, expectedOutput):
 		for i in range(self.num_inputs):
 			for j in range(self.num_outputs):
-				self.weights[i][j] += self.weightUpdate(self.in_activations[i], self.calculateError(expectedOutput, self.out_activations), self.sumInputs(j))
+				self.weights[i][j] += self.weightUpdate(self.in_activations[i], 
+				expectedOutput[j] - self.out_activations[j], self.inputSums[j])
 
 	def weightUpdate(self, activationLevel, error, inputSum):
 		return self.learning_rate * activationLevel * error * self.sigmoidPrime(inputSum)
-
-	def calculateError(self, expectedResult, outputResult):
-			difference = [x1 - x2 for (x1, x2) in zip(expectedResult, outputResult)]
-			return np.linalg.norm(difference)
 
 
 
