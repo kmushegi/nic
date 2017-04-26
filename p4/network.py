cb40f0dfc4f37b5a43601f760b61cbc54c7acc2f
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-04-26 14:17:21 -0400

    rename vars to comply with Python snakecase style

--- p4/network.py
+++ p4/network.py
@@ -7,19 +7,21 @@ import math
 class Network(object):
 	#sizes = array with number of neurons in each level: input / hidden / output
 	#[32,15,10] -> 32 input, 15 hidden and 10 output neurons
-	def __init__(self, numInputNodes, numOutputNodes, numEpochs, learningRate):
-		self.numInputNodes = numInputNodes + 1 #Add 1 for bias node
-		self.numOutputNodes = numOutputNodes
-		self.numEpochs = numEpochs
-		self.learningRate = learningRate
-		self.inputNodes = [1.0] * self.numInputNodes
-		self.outputNodes = [1.0] * self.numOutputNodes
-		self.weights = np.zeros((self.numInputNodes, self.numOutputNodes)) #np.random.rand(self.numInputNodes, self.numOutputNodes) * 0.15
+	def __init__(self, num_inputs, num_outputs, num_epochs, learning_rate):
+		self.num_inputs = num_inputs + 1 #Add 1 for bias node
+		self.num_outputs = num_outputs
+		self.num_epochs = num_epochs
+		self.learning_rate = learning_rate
+
+		self.in_activations = [1.0] * self.num_inputs
+		self.out_activations = [1.0] * self.num_outputs
+
+		self.weights = np.zeros((self.num_inputs, self.num_outputs)) #np.random.rand(self.num_inputs, self.num_outputs) * 0.15
 
 	def train(self, training_data, test_data=None):
-		for i in xrange(self.numEpochs):
+		for i in xrange(self.num_epochs):
 			for trainingSet in training_data:
-				self.initializeInputNodes(trainingSet[0])
+				self.initializein_activations(trainingSet[0])
 				self.calculateOutputNodeValues()
 				self.updateWeights(trainingSet[1])
 			print "Epoch: {0} done.".format(i)
@@ -30,24 +32,24 @@ class Network(object):
 		results = [(np.argmax(self.feedForward(x)), y) for x,y in test_data]
 		#print(results)
 
-	def feedForward(self, i):
+	def feedForward(self, inputs):
 		output = self.sigmoid(self.sumInputs(i))
 		return output
 
-	def initializeInputNodes(self, trainingInput):
+	def initializein_activations(self, trainingInput):
 		for i in range(trainingInput.size):
-			self.inputNodes[i] = 1.0 * int(trainingInput.item(i))
-		self.inputNodes[-1] = 1.0 #Bias node
+			self.in_activations[i] = 1.0 * int(trainingInput.item(i))
+		self.in_activations[-1] = 1.0 #Bias node
 
 	def calculateOutputNodeValues(self):
-		for i in range(len(self.outputNodes)):
-			self.outputNodes[i] = self.sigmoidMod(self.sumInputs(i))
-			print(self.outputNodes[i])
+		for i in range(len(self.out_activations)):
+			self.out_activations[i] = self.sigmoid(self.sumInputs(i))
+			print(self.out_activations[i])
 
 	def sumInputs(self, index):
 		inputSum = 0.0
-		for i in range(len(self.inputNodes)):
-			inputSum += self.inputNodes[i] * self.weights[i][index]
+		for i in range(len(self.in_activations)):
+			inputSum += self.in_activations[i] * self.weights[i][index]
 		return inputSum
 
 	def sigmoid(self, x):
@@ -57,12 +59,12 @@ class Network(object):
 		return self.sigmoid(x) * (1.0 - self.sigmoid(x))
 
 	def updateWeights(self, expectedOutput):
-		for i in range(self.numInputNodes):
-			for j in range(self.numOutputNodes):
-				self.weights[i][j] += self.weightUpdate(self.inputNodes[i], self.calculateError(expectedOutput, self.outputNodes), self.sumInputs(j))
+		for i in range(self.num_inputs):
+			for j in range(self.num_outputs):
+				self.weights[i][j] += self.weightUpdate(self.in_activations[i], self.calculateError(expectedOutput, self.out_activations), self.sumInputs(j))
 
 	def weightUpdate(self, activationLevel, error, inputSum):
-		return self.learningRate * activationLevel * error * self.sigmoidPrime(inputSum)
+		return self.learning_rate * activationLevel * error * self.sigmoidPrime(inputSum)
 
 	def calculateError(self, expectedResult, outputResult):
 			difference = [x1 - x2 for (x1, x2) in zip(expectedResult, outputResult)]
