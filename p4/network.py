91bc9003953e86b1ca355bc1630ea8220d1f1667
egarcia55 <egarcia@bowdoin.edu>
2017-04-26 20:32:47 -0400

    finish update. TODO for Kote: put in test() from your branch

--- p4/network.py
+++ p4/network.py
@@ -23,41 +23,31 @@ class Network(object):
 		self.weights = 0.3 * np.random.rand(self.num_inputs, self.num_outputs) - 0.15
 		self.inputSums = [1.0] * self.num_outputs
 
-	def train(self, training_data, test_data=None):
-		for i in xrange(self.num_epochs):
-			random.shuffle(training_data)
-			for index, sample in enumerate(training_data):
-				print("Sample: " + str(index), end='\r')
-				sys.stdout.flush()
-				stf = time.time()
-
-				self.feedForward(sample[0])
-
-				ef = time.time()
-
-				self.update(sample[1])
-
-				ew = time.time()
-
-				print("Update Weights: " + str(ew-ef) + "\t FF: " + str(ef-stf))
-			# print "Epoch: {0} done.".format(i)
-		if test_data:
-			self.evaluate(test_data)
-
-	def evaluate(self, test_data):
-		results = [(np.argmax(self.feedForward(x)), y) for x,y in test_data]
-		#print(results)
-
-	def feedForward(self, inputs):
-		self.in_activations[0:self.num_inputs-1] = inputs
-		self.inputSums = np.dot(self.weights.T, self.in_activations)
-		self.out_activations = self.sigmoid(self.inputSums)
-
 	def sigmoid(self, x):
 		return 1.0 / (1.0 + np.exp(-x))
 
 	def sigmoidPrime(self, x):
 		return self.sigmoid(x) * (1.0 - self.sigmoid(x))
+
+	def train(self, training_data, test_data=None):
+		for i in xrange(self.num_epochs):
+			random.shuffle(training_data)
+			for index, sample in enumerate(training_data):
+				# print("Sample: " + str(index), end='\r')
+				# sys.stdout.flush()
+
+				self.feedForward(sample[0])
+				error = self.update(sample[1])
+			print("Epoch: " + i + " finished," end='\r')
+			sys.stdout.flush()
+			print("")	
+		if test_data:
+			print("testing data was supplied")
+
+	def feedForward(self, inputs):
+		self.in_activations[0:self.num_inputs-1] = inputs
+		self.inputSums = np.dot(self.weights.T, self.in_activations)
+		self.out_activations = self.sigmoid(self.inputSums)
 
 	def update(self, desired_output):
 		error = -(desired_output - self.out_activations)
@@ -67,7 +57,7 @@ class Network(object):
 		for i in xrange(self.num_inputs):
 			for o in xrange(self.num_outputs):
 				delta = out_deltas[o] * self.in_activations[i]
-				self.weights[i][o] -= learning_rate * delta #+ delta_io[i][o]
+				self.weights[i][o] -= self.learning_rate * delta #+ delta_io[i][o]
 				#delta_io[i][o] = delta
 
 		error = 0.0
