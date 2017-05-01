65bcd228326ae89c6413ff8c9c249d688fa83878
Marcus <mefchristiansen@gmail.com>
2017-05-01 13:01:06 -0400

    comments

--- p4/src/network.py
+++ p4/src/network.py
@@ -32,7 +32,7 @@ class Network(object):
 		self.out_activations = np.ones(self.num_outputs)
 		self.delta_io = np.zeros((self.num_inputs, self.num_outputs))
 
-		#negative weights between -0.15 and 0.15
+		#Initialize weights randomly between -0.15 and 0.15
 		self.weights = 0.3 * np.random.rand(self.num_inputs, self.num_outputs) - 0.15
 		self.inputSums = np.ones(self.num_outputs)
 
@@ -50,14 +50,11 @@ class Network(object):
 
 		#for every epoch
 		for i in xrange(self.num_epochs):
-			random.shuffle(training_data) #shuffle input data and desired  outputs pairs
+			random.shuffle(training_data) #shuffle input data and desired outputs pairs
 			
 			for index, sample in enumerate(training_data): #for every input/output pair
-				#print("Sample: ",index,end='\r')
-				#sys.stdout.flush()
 				self.feedForward(sample[0])
 				self.update(sample[1])
-			#print("")
 			if test_data:
 				print("Epoch {0}:\t {1} \t {2}".format(i+1, self.test(test_data),n_test_samples))
 			else:
@@ -83,6 +80,7 @@ class Network(object):
 		if (self.num_outputs == 1): #'normalize' desired output to be in [0,1]
 			desired_output /= 10.0
 
+		#Calculate error between perceptron output and expected output
 		error = -(desired_output - self.out_activations)
 		out_deltas = self.sigmoidPrime(self.out_activations) * error
 
@@ -93,9 +91,12 @@ class Network(object):
 	#feed test_data through the neural net and return the number of correct predictions
 	def test(self, test_data):
 		if self.num_outputs == 10:
+			#'Zip' together the results from our perceptron and the expected test output to form a tuple
 			test_results = [(np.argmax(self.feedForward(x)), np.argmax(y)) for (x,y) in test_data]
 		elif self.num_outputs == 1:
+			#Here, the output of our perceptron is multiplied by 10, as our expected test output is a number [0,9]
 			test_results = [(math.floor(self.feedForward(x) * 10.0), y) for (x,y) in test_data]
+		#Return the total number of test cases that were correctly classified
 		return sum((x == y) for (x,y) in test_results)
 
 
