11de9da01520f69aafcb01242666e86d772078a8
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-05-13 14:34:05 -0400

    fix imports

--- p5/src/Runner.py
+++ p5/src/Runner.py
@@ -12,13 +12,14 @@ import network as nn
 import cnetwork as cnn
 import sys
 
-which_network = 'cnn' #of 'cnn'
+which_network = 'nn' #of 'cnn'
 nn_dataset = "cifar10" #bitmap,downsampled, cifar10
 N_EPOCHS = 50
 
 if which_network == 'nn':
 
 	#if # of CL parameters is correct take them, else go to default
+	print(len(sys.argv))
 	if len(sys.argv) == 4:
 		dataset = sys.argv[1] #bitmap, downsampled, cifar10
 		n_hid_neurons = int(sys.argv[2]) #hidden layer nodes
@@ -40,14 +41,14 @@ if which_network == 'nn':
 		n_in_neurons = 3072
 
 	#load training & test data using data_loader.py
-	(train_data, test_data) = dl.get_data(dataset,n_out_neurons)
+	(train_data, test_data) = dl.get_data(which_network,dataset,n_out_neurons)
 
 	#create an instance of the neural network with specified parameters
 	net = nn.Network(num_inputs=n_in_neurons, num_hidden=n_hid_neurons, 
 		num_outputs=n_out_neurons, num_epochs=N_EPOCHS, learning_rate=l_rate)
 
 	#start the training process
-	net.train(training_data, test_data)
+	net.train(train_data, test_data)
 
 elif which_network == 'cnn':
 	(x_train,y_train),(x_test,y_test) = dl.get_data(which_network,'cifar10')
