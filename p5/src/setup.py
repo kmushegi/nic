51e8535c0437f6527ce66acb2536128519f9c31b
Konstantine Mushegian <kmushegi@bowdoin.edu>
2017-05-13 14:54:36 -0400

    update setup.py

--- p5/src/setup.py
+++ p5/src/setup.py
@@ -1,5 +1,5 @@
 """
-Neural Networks for Digit Recognition - Project 4
+Neural Networks for Digit Recognition - Project 5
 Nature Inspired Computation
 Spring 2017
 Stephen Majercik
@@ -13,11 +13,11 @@ dependencies and store author information.
 from setuptools import setup, find_packages
 
 setup(
-	name='Neural Networks for Digit Recognition',
+	name='Project 5',
 	version='1.0',
 	description='Perceptron Artificial Neural Network',
 	url='https://github.com/kmushegi/nic/tree/master/p4', #currently private
 	author='Konstantine Mushegian',
 	author_email='kmushegian@gmail.com',
-	install_requires=['numpy'],
+	install_requires=['numpy','keras'],
 	)
